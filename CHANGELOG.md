4689e69c2a8b9a16e4173a16680b344c2c3fdc0e
Oleg Krechkovskiy <olegkrechk@gmail.com>
2023-02-25 19:09:43 +0300

    0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Change Log
+
+All notable changes to the "simple-layout-builder" extension pack will be documented in this file.
+
+## [0.2.0]
+- Fixed bugs
+## [0.1.0]
+### Added
--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -12,7 +12,7 @@ $(window).resize(function () {
   $('.block-info__height--height').text($('.main-block').height());
   $('.block-info__width--width').text($('.main-block').width());
 
-  console.log('%c Resize HEIGHT>> ', 'background:blue;color:white;', $('.main-block').height());
-  console.log('%c Resize WIDTH>> ', 'background:orange;color:black;', $('.main-block').width());
+  // console.log('%c Resize HEIGHT>> ', 'background:blue;color:white;', $('.main-block').height());
+  // console.log('%c Resize WIDTH>> ', 'background:orange;color:black;', $('.main-block').width());
 
 });
